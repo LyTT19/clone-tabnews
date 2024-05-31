2a4242d531eca52c51d2379450f67f49ef5c3f77
Jackson Callado <lytt19@hotmail.com>
2024-05-31 03:35:31 +0000

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Amor da minha vida, eu te amo demais voce e excencial para a minha existencia te amo!</h1>
+function Home() {
+  return (
+    <h1>
+      Amor da minha vida, eu te amo demais voce e excencial para a minha
+      existencia te amo!
+    </h1>
+  );
 }
 
 export default Home;
